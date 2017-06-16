0a6678596621f8a9602fae95437788aa4ce0e31c
madvas <matus.lestan@gmail.com>
2017-06-17 02:06:02 +0700

    Remove test-compiled folder

--- test-compiled/cljs_web3/net.cljs
+++
@@ -1,5 +0,0 @@
-(ns cljs-web3.net
-  (:require [cljs-web3.utils :as u]))
-
-(def listening? (u/prop-or-clb-fn "net" "listening"))
-(def peer-count (u/prop-or-clb-fn "net" "peerCount"))
--- test-compiled/cljs_web3/net.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.9.227 {:target :nodejs}
-goog.provide('cljs_web3.net');
-goog.require('cljs.core');
-goog.require('cljs_web3.utils');
-cljs_web3.net.listening_QMARK_ = cljs_web3.utils.prop_or_clb_fn.call(null,"net","listening");
-cljs_web3.net.peer_count = cljs_web3.utils.prop_or_clb_fn.call(null,"net","peerCount");
